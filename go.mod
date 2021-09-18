e3291de2e2f5ac6218cd4714bda168a24257109b
Toan Vo <toanalien@gmail.com>
2021-09-18 21:39:05 +0700

    Add github.com/olekukonko/tablewriter

--- go.mod
+++ go.mod
@@ -3,25 +3,46 @@ module executor
 go 1.17
 
 require (
+	cloud.google.com/go/firestore v1.1.0
 	github.com/adshao/go-binance/v2 v2.3.1
 	github.com/spf13/viper v1.8.1
 )
 
 require (
+	cloud.google.com/go v0.81.0 // indirect
 	github.com/bitly/go-simplejson v0.5.0 // indirect
 	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.5 // indirect
+	github.com/googleapis/gax-go/v2 v2.0.5 // indirect
 	github.com/gorilla/websocket v1.4.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/jstemmer/go-junit-report v0.9.1 // indirect
 	github.com/magiconair/properties v1.8.5 // indirect
+	github.com/mattn/go-runewidth v0.0.9 // indirect
 	github.com/mitchellh/mapstructure v1.4.1 // indirect
+	github.com/olekukonko/tablewriter v0.0.5 // indirect
 	github.com/pelletier/go-toml v1.9.3 // indirect
 	github.com/spf13/afero v1.6.0 // indirect
 	github.com/spf13/cast v1.3.1 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.2.0 // indirect
+	go.opencensus.io v0.23.0 // indirect
+	golang.org/x/lint v0.0.0-20210508222113-6edffad5e616 // indirect
+	golang.org/x/mod v0.4.2 // indirect
+	golang.org/x/net v0.0.0-20210405180319-a5a99cb37ef4 // indirect
+	golang.org/x/oauth2 v0.0.0-20210402161424-2e8d93401602 // indirect
 	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
 	golang.org/x/text v0.3.5 // indirect
+	golang.org/x/tools v0.1.2 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	google.golang.org/api v0.44.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20210602131652-f16073e35f0c // indirect
+	google.golang.org/grpc v1.38.0 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
 	gopkg.in/ini.v1 v1.62.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -4,8 +4,12 @@ import (
 	"context"
 	"fmt"
 	"github.com/adshao/go-binance/v2"
+	"github.com/olekukonko/tablewriter"
 	"github.com/spf13/viper"
+	"os"
+	"sort"
 	"strconv"
+	"time"
 )
 
 func main() {
@@ -17,15 +21,71 @@ func main() {
 	)
 	ctx := context.Background()
 
-	client := binance.NewClient(apiKey, secretKey)
-	margin, _ := client.NewGetIsolatedMarginAccountService().Do(ctx)
-	userAssets := margin.Assets
+	binanceClient := binance.NewClient(apiKey, secretKey)
+	isolatedMargin, _ := binanceClient.NewGetIsolatedMarginAccountService().Do(ctx)
+	userAssets := isolatedMargin.Assets
 
-	for _, asset := range userAssets {
-		liquidatePrice, err := strconv.ParseFloat(asset.LiquidatePrice, 64)
-		if (err == nil) && (liquidatePrice != 0) {
-			fmt.Printf("%s %f\n", asset.Symbol, liquidatePrice)
+	sort.Slice(userAssets, func(i, j int) bool {
+		baseAssetNetAssetOfBtc, _ := strconv.ParseFloat(userAssets[i].BaseAsset.NetAssetOfBtc, 64)
+		quoteAssetNetAssetOfBtc, _ := strconv.ParseFloat(userAssets[i].QuoteAsset.NetAssetOfBtc, 64)
+		netAssetOfBtcI := baseAssetNetAssetOfBtc + quoteAssetNetAssetOfBtc
+		baseAssetNetAssetOfBtc, _ = strconv.ParseFloat(userAssets[j].BaseAsset.NetAssetOfBtc, 64)
+		quoteAssetNetAssetOfBtc, _ = strconv.ParseFloat(userAssets[j].QuoteAsset.NetAssetOfBtc, 64)
+		netAssetOfBtcJ := baseAssetNetAssetOfBtc + quoteAssetNetAssetOfBtc
+		return netAssetOfBtcI > netAssetOfBtcJ
+	})
+
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"Symbol", "QuoteAsset", "BaseAsset", "LiquidatePrice"})
+	for _, v := range userAssets {
+		liquidatePrice, _ := strconv.ParseFloat(v.LiquidatePrice, 64)
+		quoteAsset, _ := strconv.ParseFloat(v.QuoteAsset.TotalAsset, 64)
+		baseAsset, _ := strconv.ParseFloat(v.BaseAsset.TotalAsset, 64)
+		if liquidatePrice != 0 || quoteAsset != 0 || baseAsset != 0 {
+			table.Append([]string{v.Symbol, v.QuoteAsset.NetAsset, v.BaseAsset.NetAsset, v.LiquidatePrice})
 		}
+	}
+	table.Render()
+
+	isolatedMarginOpenOrders, err := binanceClient.NewListMarginOpenOrdersService().IsIsolated(true).Symbol("BTCUSDT").Do(ctx)
+
+	if err != nil {
 
 	}
+
+	sort.Slice(isolatedMarginOpenOrders, func(i, j int) bool {
+		return isolatedMarginOpenOrders[i].Time > isolatedMarginOpenOrders[j].Time
+	})
+
+	table = tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"Symbol", "ExecutedQuantity", "QuoteQuantity", "Side", "Status", "Type", "TimeInForce"})
+	for _, v := range isolatedMarginOpenOrders {
+		table.Append([]string{v.Symbol, v.ExecutedQuantity, v.CummulativeQuoteQuantity, string(v.Side), string(v.Status), string(v.Type), time.UnixMilli(v.Time).Format("2006-01-02 15:04:05 -0700")})
+	}
+	table.Render()
+
+	isolatedMarginTrades, err := binanceClient.NewListMarginTradesService().IsIsolated(true).Symbol("BTCUSDT").Do(ctx)
+	if err != nil {
+
+	}
+
+	table = tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"Symbol", "IsBuyer", "Price", "CommissionAsset", "Commission", "Quantity", "QuoteQuantity", "Time"})
+
+	sort.Slice(isolatedMarginTrades, func(i, j int) bool {
+		return isolatedMarginTrades[i].Time > isolatedMarginTrades[j].Time
+	})
+
+	for _, v := range isolatedMarginTrades {
+		side := "SELL"
+		if v.IsBuyer == true {
+			side = "BUY"
+		}
+		quantity, _ := strconv.ParseFloat(v.Quantity, 64)
+		price, _ := strconv.ParseFloat(v.Price, 64)
+		quoteQuantity := quantity * price
+		table.Append([]string{v.Symbol, side, v.Price, v.CommissionAsset, v.Commission, v.Quantity, fmt.Sprintf("%f", quoteQuantity), time.UnixMilli(v.Time).Format("2006-01-02 15:04:05 -0700")})
+	}
+	table.Render()
+
 }
