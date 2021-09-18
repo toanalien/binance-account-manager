3229cc853ead5e65f72fae99679573dfaa493223
Toan Vo <toanalien@gmail.com>
2021-09-18 12:33:18 +0700

    WIP: get liquidated price

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/adshao/go-binance/v2"
 	"github.com/spf13/viper"
+	"strconv"
 )
 
 func main() {
@@ -21,14 +22,10 @@ func main() {
 	userAssets := margin.Assets
 
 	for _, asset := range userAssets {
-		fmt.Println(asset)
-	}
+		liquidatePrice, err := strconv.ParseFloat(asset.LiquidatePrice, 64)
+		if (err == nil) && (liquidatePrice != 0) {
+			fmt.Printf("%s %f\n", asset.Symbol, liquidatePrice)
+		}
 
-	res, err := client.NewGetAccountService().Do(ctx)
-	if err != nil {
-		fmt.Println(err)
-		return
 	}
-	fmt.Println(res)
-
 }
